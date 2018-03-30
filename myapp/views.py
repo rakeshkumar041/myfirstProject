bf2d22b7abc0c0d876d748d252cbbe52a3e40680
rakeshkumar041 <rockyrakesh041.ece@gmail.com>
2018-03-30 18:03:37 +0530

    adding Templates

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-   text = """<h1>welcome to my app !</h1>"""
-   return HttpResponse(text)
+#def hello(request):
+ #  text = """<h1>welcome to my app !</h1>"""
+ #  return HttpResponse(text)
+   
+def hello(request):  
+   return render(request, 'myapp/myapp_list.html', {})
